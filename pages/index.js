7dc2511b75e75c89c3ffe9201f81317d94297e7f
sohailB <bhattisohail20@gmail.com>
2023-05-10 20:10:39 +0500

    code

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@
 // import SimpleCard from "../component/cards/SimpleCard";
 import algoliasearch from "algoliasearch/lite";
 import {
-  ClearRefinements,
-  Configure,
-  Hits,
-  InstantSearch,
-  Pagination,
-  RefinementList,
-  SearchBox,
+    ClearRefinements,
+    Configure,
+    Hits,
+    InstantSearch,
+    Pagination,
+    RefinementList,
+    SearchBox,
 } from "react-instantsearch-dom";
 // import "../styles/Home.module.css";
 import { HitWithInsights } from "./HitWithInsights";
@@ -19,30 +19,34 @@ import { HitWithInsights } from "./HitWithInsights";
 // import SearchPage from "./SearchPage";
 
 const searchClient = algoliasearch(
-  "QHYLAXTG31",
-  "babc39e7f16ddece7fd1a1848312ff0e"
+    "QHYLAXTG31",
+    "babc39e7f16ddece7fd1a1848312ff0e"
 );
 
 export default function Home() {
-  
-  return (
-    <>
-     <div className="ais-InstantSearch">
-        <h1>Shopping</h1>
-        <InstantSearch indexName="dev_test" searchClient={searchClient}>
-          <div className="left-panel">
-            <ClearRefinements />
-            <h2>Brands</h2>
-            <RefinementList attribute="postType.en" />
-            <Configure hitsPerPage={2} clickAnalytics />
-          </div>
-          <div className="right-panel">
-            <SearchBox />
-            <Hits hitComponent={HitWithInsights} />
-            <Pagination />
-          </div>
-        </InstantSearch>
-      </div>
-    </>
-  );
+
+    return (
+        <>
+            <div className="ais-InstantSearch">
+                <h1>Shopping</h1>
+                <InstantSearch indexName="dev_test" searchClient={searchClient}>
+                    <div className="left-panel">
+                        <ClearRefinements />
+                        <h2>Brands</h2>
+                        <RefinementList attribute="postType.en" />
+                        <Configure hitsPerPage={2} clickAnalytics />
+                    </div>
+                    <div className="right-panel" style={{ marginTop: 20 }}>
+                        <div style={{
+                            cursor: "pointer", marginTop: 0
+                        }}>
+                            <SearchBox />
+                        </div>
+                        <Hits hitComponent={HitWithInsights} />
+                        <Pagination />
+                    </div>
+                </InstantSearch>
+            </div>
+        </>
+    );
 }
